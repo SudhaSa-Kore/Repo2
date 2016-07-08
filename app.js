cfcc6cea41c6db1b37fde148662ab777432a5611
Kstream 001 <kstream001@gmail.com>
2016-07-08 16:16:55 +0530

    FBmessenger

--- app.js
+++ app.js
@@ -1,81 +1,128 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-app.get('/index',function(req,res){
-	res.sendFile(path.join(__dirname+'/index.html'));
-});
-
-
-app.get('/getPremiumDate',function(req,res){
-	res.writeHead(200,{'Content-Type':'application/json'});
-	res.end(JSON.stringify({"policies":[{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/5/2019","premiumPaymentDate":"27/5/2017"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"},{"policyNumber":"1987650","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"2/6/2016"}]}));
-});
-
-
-app.get('/payPremiumamount',function(req,res){
-	res.writeHead(200,{'Content-Type':'application/json'});
-	res.end(JSON.stringify({"policyNumber":"9348209","message":"Successfully paid the premium amount","nextPremiumDate":"27/5/2018"}));
-});
-
-app.get('/getTaxCertificate',function(req,res){
-	res.writeHead(200,{'Content-Type':'application/json'});
-	res.end(JSON.stringify({"policyNumber":"9348209","policyName":"Jeevan Anandh","url":"http://www.revenue.ie/images/guide-paye-employees-booklet.jpg"}));
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+var express = require('express')
+var bodyParser = require('body-parser')
+var request = require('request')
+var app = express()
+var Client = require('node-rest-client').Client;
+var client = new Client();
+ 
+ 
+app.set('port', (process.env.PORT || 5000))
+ 
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+ 
+// Process application/json
+app.use(bodyParser.json())
+ 
+// Index route
+app.get('/', function (req, res) {
+    res.send('Hello world, I am a chat bot')
+})
+ 
+// for Facebook verification
+app.post('/webhook/', function (req, res) {
+   
+    console.log("Req params: entry 0 " + JSON.stringify(req.body.entry[0]) )
+   
+    messaging_events = req.body.entry[0].messaging
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i]
+        sender = event.sender.id
+      
+        console.log("Sender Id : " + sender)
+       
+        if (event.postback) {
+            console.log ("Payload : " + event.postback.payload)
+            payload = JSON.parse(event.postback.payload);
+            sendTextMessage(payload.id, "Someone is following you ","T")
+           
+        }
+        if (event.message && event.message.text) {
+            text = event.message.text
+            url = "https://dry-falls-30126.herokuapp.com/getPremiumDate"
+            client.get(url, function (data, response) {
+               
+               console.log(data);
+                   text= {
+       "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+          {
+            "title":"Policies with Premium Due Dates",
+            "image_url":"https://3.bp.blogspot.com/-CSVO5tqjKPk/Vx-AFGW47MI/AAAAAAAAALo/XTvdYqh76Es205Glo7kYUO9iwucMNOnxwCLcB/s640/income%2Btax%2Brules%2Bfor%2Binsurance%2Bpolicies.png",
+            "subtitle":data.policies[i].policyName,
+            "buttons":[
+              {               
+                "type":"postback",
+                "title":data.policies[0].policyName,
+                "payload":"{\"id\":\"10153635012668441\"}"
+              },       
+              {
+                        
+                "type":"postback",
+                "title":data.policies[0].policyType,
+                "payload":"{\"id\":\"10153807636891219\"}"
+              },
+              {
+                 "type":"postback",
+                "title":data.policies[0].policyNumber,
+                "payload":"{\"id\":\"1081292718609707\"}"
+              }             
+            ]
+          }
+                
+            ]
+          }
+      }
+            sendTextMessage(sender, text,type)
+        });
+    }
+    res.sendStatus(200)
+    }
+})
+ 
+var token = "EAARc6kfSVyoBAMQm1Asgtk86CL2XBAImEaVIPVhCP9ZCMJSYfqdmrJALWGqe4GreJxA3hLBxAgsENfGtdZCXgv1ZCSDYXTDqQeDZCCjDs3yZA5z6VT2srZBbbuJEWHkEAkEiTlyZCU31oqQZAEdFqkic3C4ZCCtTXQDLqFWFk2Vy98OwKn52Ccuvh"
+ 
+function sendTextMessage(sender, text,type) {
+   
+    if (type=="A")
+    {
+        messageData =
+        {
+            "attachment":text
+      
+         }
+    }
+    else
+    {
+      messageData =
+        {
+            text:text
+        }  
+    }
+ 
+ 
+   
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
 }
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+ 
+ 
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
---
+++ app_test.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+app.get('/getPremiumDate',function(req,res){
+	res.writeHead(200,{'Content-Type':'application/json'});
+	res.end(JSON.stringify({"policies":[{"policyNumber":"9348209","policyStartDate":"27/5/2016","policyType":"yearly","policyName":"Jeevan Anandh","premiumAmount":"31,000","policyEndDate":"27/5/2019","premiumPaymentDate":"27/5/2017"},{"policyNumber":"1987650","policyStartDate":"12/2/2013","policyType":"half-yearly","policyName":"Jeevan Tarang","premiumAmount":"20,000","policyEndDate":"27/5/2018","premiumPaymentDate":"12/8/2016"},{"policyNumber":"1987650","policyStartDate":"2/7/2007","policyType":"monthly","policyName":"Abhinay","premiumAmount":"20,000","policyEndDate":"2/7/2020","premiumPaymentDate":"2/6/2016"}]}));
+});
+
+
+app.get('/payPremiumamount',function(req,res){
+	res.writeHead(200,{'Content-Type':'application/json'});
+	res.end(JSON.stringify({"policyNumber":"9348209","message":"Successfully paid the premium amount","nextPremiumDate":"27/5/2018"}));
+});
+
+app.get('/getTaxCertificate',function(req,res){
+	res.writeHead(200,{'Content-Type':'application/json'});
+	res.end(JSON.stringify({"policyNumber":"9348209","policyName":"Jeevan Anandh","url":"http://www.revenue.ie/images/guide-paye-employees-booklet.jpg"}));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
